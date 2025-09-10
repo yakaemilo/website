9628c65fcae83d844aaf7eb7462bd51fbc700e4b
yakaemilo <yakaemilo@mailum.com>
2025-09-10 12:51:35 +0200

    email

--- replace.bash
+++ replace.bash
@@ -9,13 +9,21 @@ read -r location
 echo "enter age "
 read -r age
 
+echo "enter phone number "
+read -r number
+
+echo "enter email "
+read -r email
+
 sed -i "s/##LOCATION/$location/g" *
 sed -i "s/##NAME/$name/g" *
 sed -i "s/##AGE/$age Jahre/g" *
-
-
+sed -i "s/##NUMBER/$number/g" *
+sed -i "s/##EMAIL/$email/g" *
 
 
 unset location
 unset name
 unset age
+unset number
+unset email
